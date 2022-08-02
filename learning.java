818b1c207d3f6d85050c92af9f5969b41bed5ce1
JustTyping1 <aditya.chorghade@outlook.com>
2022-08-02 15:54:11 +0100

    function

--- learning.java
+++ learning.java
@@ -1,5 +1,9 @@
 public class learning {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println(func(7, 4));
+    }
+    public static int func(int a, int b){
+        return a+b;
     }
 }
